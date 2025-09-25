a26b06ba2fa6fc32b26d39c2aa8d740b2931a5cb
Antriel <antoine.nusbaum06@gmail.com>
2025-09-25 14:16:54 +0200

    Réctification car this.joueurActuel n'était pas défini

--- src/partie.ts
+++ src/partie.ts
@@ -1,28 +1,28 @@
-import { Grille } from "./Grille";
-import { Joueur } from "./Joueur";
+import { Grille } from "./grille";
+import { Joueur } from "./joueur";
 
 export class Partie {
   private grille: Grille;
   private joueurs: Joueur[];
-  private joueurActuel: Joueur;
+  private joueurActuel!: Joueur; // "!" = sera défini dans le constructeur
 
   constructor() {
     this.grille = new Grille();
     this.joueurs = [new Joueur(1, "X"), new Joueur(2, "O")];
-    this.joueurActuel = this.joueurs[0];
+    this.joueurActuel = this.joueurs[0]!; // "!" = pas undefined
   }
 
-  changerJoueur() {
-    this.joueurActuel = 
-      this.joueurActuel.id === 1 ? this.joueurs[1] : this.joueurs[0];
+  changerJoueur(): void {
+    this.joueurActuel =
+      this.joueurActuel.id === 1 ? this.joueurs[1]! : this.joueurs[0]!;
   }
 
   jouer(colonne: number): boolean {
     const plateau = this.grille.getPlateau();
 
     for (let i = plateau.length - 1; i >= 0; i--) {
-      if (plateau[i][colonne] === 0) {
-        plateau[i][colonne] = this.joueurActuel.id;
+      if (plateau[i]![colonne] === 0) {
+        plateau[i]![colonne] = this.joueurActuel.id;
         return true;
       }
     }
