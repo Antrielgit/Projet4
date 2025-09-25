a164d0939370740fceb0c70f1055c5e707168bcb
Antriel <antoine.nusbaum06@gmail.com>
2025-09-25 15:26:25 +0200

    Continuation pour tester le script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { Partie } from "./partie";
 
 const partie = new Partie();
-partie.jouer(3);
-partie.jouer(4);
+
+console.log("🎮 Début de la partie !");
+partie.jouer(3); // Joueur 1 joue dans la colonne 3
+partie.jouer(3); // Joueur 2 joue dans la colonne 3
+partie.jouer(2); // Joueur 1 joue dans la colonne 2
+
+console.log("✅ Tests terminés !");
